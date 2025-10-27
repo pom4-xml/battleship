9e057b298475295a17a28f635c5a371575aead74
Mikel Landa <mikel.landad@alumni.mondragon.com>
2025-10-27 12:38:49 +0100

    [mikel] First Socket Logic

--- game/src/main/java/battleship/Main.java
+++ game/src/main/java/battleship/Main.java
@@ -1,7 +1,26 @@
 package battleship;
 
+import java.util.Scanner;
+
+import battleship.socket.Client;
+import battleship.socket.Server;
+
 public class Main {
+    static Scanner scanner;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        scanner = new Scanner(System.in);
+        System.out.println("Choose your role: ");
+        System.out.println("0 - Server");
+        System.out.println("1 - Client");
+        int option = Integer.parseInt(scanner.nextLine());
+        
+        if (option == 0) {  
+            Server server = new Server(5000);
+            server.start();
+        }
+        else if (option == 1) {
+            Client client = new Client("127.0.0.1", 5000);
+            client.start();
+        }
     }
 }
