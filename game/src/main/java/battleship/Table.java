8b30ef8af088d174a951a5e9c2d9f67b67bd6f5d
Edu <eduardo.santoyo@alumni.mondragon.edu>
2025-10-27 12:22:34 +0100

    tableV2

--- game/src/main/java/battleship/Table.java
+++ game/src/main/java/battleship/Table.java
@@ -1,12 +1,27 @@
 package battleship;
 
+import java.util.List;
 
 public class Table {
     private static final int SIZE = 10;
     private int [][] matrix;
+    
 
     public Table(){
         matrix = new int[SIZE][SIZE];
+    }
+
+    public boolean checkRivalShot(Position position,List<Ship> myListOfShips){
+        if(position == null) return false;
+        if(myListOfShips == null || myListOfShips.isEmpty()) return false;
+        for(Ship ship : myListOfShips){
+            if(ship.getPositions().contains(position)){
+                matrix [position.getX()][position.getY()] = 2;
+                return true;
+            }
+        }
+        matrix [position.getX()][position.getY()] = 1;
+        return false;
     }
 
     public int[][] getMatrix() {
