e1154562b97e1bff4957428048045db18902773d
leireTo <leiretoledanodieguez@gmail.com>
2025-10-27 11:37:40 +0100

    Ships and variants created, no testing.

---
+++ game/src/main/java/battleship/Carrier.java
@@ -0,0 +1,7 @@
+package battleship;
+
+public class Carrier extends Ship {
+    public Carrier() {
+        super(5);
+    }
+}
---
+++ game/src/main/java/battleship/Ship.java
@@ -0,0 +1,58 @@
+package battleship;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public abstract class Ship {
+    int size;
+    List<Position> positions;
+    Set<Position> hits;
+
+    public Ship(int size) {
+        this.size = size;
+        this.hits = new HashSet<>();
+    }
+    
+    public boolean occupies(Position p) {
+        return positions.contains(p);
+    }
+
+    public boolean registerHit(Position p) {
+        if (occupies(p)) {
+            hits.add(p);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSunk() {
+        return hits.size() == size;
+    }
+
+    // Getters and Setters
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<Position> positions) {
+        this.positions = positions;
+    }
+
+    public Set<Position> getHits() {
+        return hits;
+    }
+
+    public void setHits(Set<Position> hits) {
+        this.hits = hits;
+    }
+}
