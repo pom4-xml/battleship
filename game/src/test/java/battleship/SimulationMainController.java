852ff1a4fb207a972ef376d344e551779674c158
leireTo <leiretoledanodieguez@gmail.com>
2025-10-30 12:31:18 +0100

    tests: table, simulationmain, simulationcontroller, board, ship

--- game/src/test/java/battleship/SimulationMainController.java
+++ game/src/test/java/battleship/SimulationMainController.java
@@ -1,5 +1,37 @@
 package battleship;
 
-public class SimulationMainController {
-    
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class SimulationGameControllerTest {
+
+    private SimulationGameController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SimulationGameController();
+    }
+
+    @Test
+    void testGameRunsWithSimulatedInput() {
+        String input = ""
+                + "0 0 true\n"
+                + "1 0 true\n"
+                + "0 0 true\n"
+                + "1 0 true\n"
+                + "0 0\n"
+                + "0 0\n";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        controller.startGame();
+
+        assertNotNull(controller, "Controller se inicializó correctamente");
+    }
 }
