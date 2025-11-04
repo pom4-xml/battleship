23dadfe07abd5d96109654a7eab8e4d19f9c6200
leireTo <leiretoledanodieguez@gmail.com>
2025-11-04 10:41:27 +0100

    Sim main and controller issues solved hopefullyaaaaaaa

--- game/src/test/java/battleship/SimulationMainControllerTest.java
+++ game/src/test/java/battleship/SimulationMainControllerTest.java
@@ -2,22 +2,22 @@ package battleship;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class SimulationGameControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "0 0 true\n1 0 true\n0 0 true\n1 0 true\n0 0\n0 0\n1 0\n1 0\n",
-            "0 1 true\n2 2 true\n0 1 true\n2 2 true\n1 1\n1 0\n2 2\n2 2\n"
+            "0 0 true\n0 1 true\n5 5 false\n5 6 false\n0 0\n5 5\n",
+            "1 0 true\n1 1 false\n6 6 true\n6 7 false\n1 0\n6 6\n"
     })
     void testControllerInitializes(String inputSequence) {
         InputStream originalIn = System.in;
-        System.setIn(new ByteArrayInputStream(inputSequence.getBytes()));
+        ByteArrayInputStream bais = new ByteArrayInputStream(inputSequence.getBytes());
+        System.setIn(bais);
+
         try {
             SimulationGameController controller = new SimulationGameController();
             controller.startGame();
--- game/src/test/java/battleship/SimulationMainTest.java
+++ game/src/test/java/battleship/SimulationMainTest.java
@@ -1,10 +1,8 @@
 package battleship;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class SimulationMainTest {
@@ -13,16 +11,16 @@ class SimulationMainTest {
     void testMainRunsWithoutException() {
         String simulatedInput =
                 "0 0 true\n" +
-                "1 0 true\n" +
-                "0 0 true\n" +
-                "1 0 true\n" +
+                "0 1 true\n" +
+                "5 5 false\n" +
+                "5 6 false\n" +
                 "0 0\n" +
-                "0 0\n" +
-                "1 0\n" +
-                "1 0\n";
+                "5 5\n";
 
         InputStream originalIn = System.in;
-        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+        ByteArrayInputStream bais = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(bais);
+
         try {
             assertDoesNotThrow(() -> SimulationMain.main(new String[]{}),
                     "The main method should run without throwing exceptions");
