20cd5a7b32c3e2a5f93fd7aa6ab5c96cdedd2c74
leireTo <leiretoledanodieguez@gmail.com>
2025-11-04 10:32:40 +0100

    por fin try con resource

--- game/src/test/java/battleship/SimulationMainTest.java
+++ game/src/test/java/battleship/SimulationMainTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -14,8 +15,10 @@ class SimulationMainTest {
         String simulatedInput = "0 0 true\n1 0 true\n0 0 true\n1 0 true\n0 0\n0 0\n";
         InputStream originalIn = System.in;
 
-        try {
-            System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+        ByteArrayInputStream bais = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(bais);
+
+        try (Scanner scanner = new Scanner(System.in)) {
             assertDoesNotThrow(() -> SimulationMain.main(new String[]{}));
         } finally {
             System.setIn(originalIn);
